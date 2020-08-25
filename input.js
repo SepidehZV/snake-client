ab4028815b8d5fa8d024d5d71df616b99d1baae3
sepideh zamanvaziri <zamanvaziri.sepideh@gmail.com>
2020-08-25 21:19:58 +0000

    sending input data to the server and control the snake moves!

--- input.js
+++ input.js
@@ -1,3 +1,4 @@
+let connection;
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
@@ -7,15 +8,34 @@ const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
   };
-}
-  
-const setupInput = function() {
+};
+//const moves = ["up",'left','down','right']; 
+//const keys = ['w','a','s','d']; 
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   //handleUserInput function to exit with ^c
   stdin.on('data',handleUserInput );
+
+  stdin.on('data',(key) => {
+    switch (key) {
+      case 'w': 
+        connection.write("Move: up");
+        break;
+      case 'a': 
+      connection.write("Move: left");
+        break;
+      case 's':
+        connection.write("Move: down");
+        break;
+      case 'd':
+        connection.write("Move: right");
+        break;
+    };
+  } );
   return stdin;
 };
 module.exports = {setupInput};
