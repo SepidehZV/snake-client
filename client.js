4b60925fd09624d4ed644810ef05473116def0c8
sepideh zamanvaziri <zamanvaziri.sepideh@gmail.com>
2020-08-25 17:46:52 +0000

    experimented with the move up command

--- client.js
+++ client.js
@@ -18,7 +18,25 @@ const connect = function() {
   });
   //print on the client that we are connected
   conn.on('connect', () => {
-    console.log("Successfully connected to game server")
+    console.log("Successfully connected to game server");
+    /*
+    //moves snake up with setIntervals
+    const myMove = setInterval(() => {
+      conn.write("Move: up");
+    },50);*/
+    
+    /*
+    //moves th snake up 5 times right after connection establishes each have 50ms delay
+    let timeDelay = 50;
+    for (let i = 0; i < 5; ++i) {
+      setTimeout(() => conn.write("Move: up"), timeDelay);
+      timeDelay += 50;
+    };*/
+
+    /*
+    //moves the snake up for one block as soon as we connect
+    conn.write("Move: up");*/
+    
   });
   //sending our name to the server
   conn.write("Name: SZV")
