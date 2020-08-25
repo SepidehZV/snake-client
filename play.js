96bff0c183603149bc40bbb5571879f9062058cc
sepideh zamanvaziri <zamanvaziri.sepideh@gmail.com>
2020-08-25 20:29:41 +0000

    handler UserInput function

--- play.js
+++ play.js
@@ -7,16 +7,19 @@ connect();
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
+//handleUserInput function to exit with ^c
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    process.exit();
+  };
+}
+  
 const setupInput = function() {
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
   //handleUserInput function to exit with ^c
-  stdin.on('data', (key) => {
-    if (key === '\u0003') {
-      process.exit();
-    };
-  });
+  stdin.on('data',handleUserInput );
   return stdin;
 };
